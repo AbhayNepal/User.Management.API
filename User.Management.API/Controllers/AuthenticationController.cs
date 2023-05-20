a6bc288d1532da4a9c526d77a523e8755f1530fb
Abhay Nepal <abhay.nepal.9@gmail.com>
2023-05-20 09:32:04 +0545

    Implemented token for each login and login expiry.

--- User.Management.API/Controllers/AuthenticationController.cs
+++ User.Management.API/Controllers/AuthenticationController.cs
@@ -6,6 +6,11 @@ using User.Management.API.Models.Authentication.Login;
 using User.Management.API.Models.Authentication.Signup;
 using User.Management.Service.Models;
 using User.Management.Service.Services;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace User.Management.API.Controllers
 {
@@ -57,7 +62,7 @@ namespace User.Management.API.Controllers
 
                 //Add Token to Verify the Email...
                 var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                var confirmationLink = Url.Action(nameof(ConfirmEmail), "Authentication", new { token, email=user.Email },Request.Scheme);
+                var confirmationLink = Url.Action(nameof(ConfirmEmail), "Authentication", new { token, email = user.Email }, Request.Scheme);
                 var message = new Message(new string[] { user.Email! }, "Confirmation email link", confirmationLink!);
                 _emailService.SendEmail(message);
 
@@ -91,7 +96,58 @@ namespace User.Management.API.Controllers
 
         }
         //adding a comment for commit
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
+        {
+            //checking the user
+            var user = await _userManager.FindByNameAsync(loginModel.Username);
+            //checking the password
+            if (user != null && await _userManager.CheckPasswordAsync(user, loginModel.Password))
+            {
+                //claimlist creation
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
 
+                };
+
+                //we add role to the claim list
+                var userRoles = await _userManager.GetRolesAsync(user);
+                foreach (var role in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, role));
+                }
+                //generate the token with the names
+                var jwtToken = GetToken(authClaims);
+
+                //returning the token
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(jwtToken),
+                expiration = jwtToken.ValidTo
+                });
+                
+               
+            }
+            return Unauthorized();
+
+
+        }
+        private JwtSecurityToken GetToken(List<Claim> authClaims)
+        {
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+            var token = new JwtSecurityToken(
+            issuer: _configuration["JWT:ValidIssuer"],
+            audience: _configuration["JWT:ValidIssuer"],
+            expires: DateTime.Now.AddHours(1),
+            claims: authClaims,
+            signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+            );
+            return token;
+
+        }
     }
 }
 
--- User.Management.API/Models/Authentication/Login/LoginModel.cs
+++ User.Management.API/Models/Authentication/Login/LoginModel.cs
@@ -1,6 +1,13 @@
-﻿namespace User.Management.API.Models.Authentication.Login
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace User.Management.API.Models.Authentication.Login
 {
     public class LoginModel
     {
+        [Required(ErrorMessage = "User Name is Required")]
+        public string? Username { get; set; }
+        [Required(ErrorMessage ="Password is Required")]
+        public string? Password { get; set; }
+
     }
 }
